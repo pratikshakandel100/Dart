a9f41238f6aea6b84589114f5fc5915860483178
unknown <pratikshakandel100@gmail.com>
2026-04-05 22:52:24 +0545

    collection

---
+++ dart_application_1/lib/question/q1.dart
@@ -0,0 +1,7 @@
+void main(){
+  List<String> arr = ["Sony", "Sristika", "Unisha", "Sandhya"];
+  print(arr[0]);
+  print(arr[1]);
+  print(arr[2]);
+  print(arr[3]);
+}
---
+++ dart_application_1/lib/question/q2.dart
@@ -0,0 +1,6 @@
+void main(){
+  Set<String> fruits = {"Apple", "Banana", "Pineapple", "Avogado"};
+  for(String fruit in fruits){
+    print(fruit);
+  }
+}
